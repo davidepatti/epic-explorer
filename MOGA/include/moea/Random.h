ee6a33e3d67d46a956eb2cb6424349c3700f48a8
adinuovo <adinuovo@07f4231e-1d56-586e-1340-b6309ebb75fe>
2006-04-19 13:39:30 +0000

    - user defined random seed in GA exploration

--- MOGA/include/moea/Random.h
+++ MOGA/include/moea/Random.h
@@ -5,21 +5,21 @@
 #define _RANDOM_H
 
 extern unsigned long Seed, originalSeed;
+#include <iostream>
+using namespace std;
 
 /* random number generator (copied from Grefensttete's genesis) */
 
 #define MASK 2147483647
 #define PRIME 65539
 #define SCALE 0.4656612875e-9
-#define DEFAULT_SEED   123456789
+//#define DEFAULT_SEED   123456789
 /****************************************************************/
 /*	 Rand computes a psuedo-random				*/
 /*	 double value between 0 and 1, excluding 1.  Randint	*/
 /*	 gives an integer value between low and high inclusive.	*/
 /****************************************************************/
-#define Rand()  (( Seed = ( (Seed * PRIME) & MASK) ) * SCALE )
-#define Randint(low,high) ( (int) ((low) + ((high)-(low)+1) * Rand()))
-
-
+double Rand();// {cout<<"\n\n Seed = "<<Seed<<endl; cin>>Seed; return ((( (Seed * PRIME) & MASK) ) * SCALE ); }
+int Randint(int low, int high);// { return ((int) ((low) + ((high)-(low)+1) * Rand()));}
 
 #endif
