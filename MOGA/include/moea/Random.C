d7452b2223941ce102340ce23d566d6bb8d790ab
adinuovo <adinuovo@07f4231e-1d56-586e-1340-b6309ebb75fe>
2006-04-19 14:43:10 +0000

    - fixed random generation code

--- MOGA/include/moea/Random.C
+++ MOGA/include/moea/Random.C
@@ -1,7 +1,4 @@
 #include "Random.h"
 
-unsigned long Seed = 0;//DEFAULT_SEED;
-unsigned long originalSeed = 0;//DEFAULT_SEED;
-
-double Rand() {return ((( (Seed * PRIME) & MASK) ) * SCALE ); }
-int Randint(int low, int high) { return ((int) ((low) + ((high)-(low)+1) * Rand()));}
+unsigned long Seed = DEFAULT_SEED;
+unsigned long originalSeed = DEFAULT_SEED;
