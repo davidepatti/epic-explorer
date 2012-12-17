90d04020cbf1c6dce82f6ae1fd56433ba58f526f
xedivad@gmail.com <xedivad@gmail.com@07f4231e-1d56-586e-1340-b6309ebb75fe>
2012-12-17 13:33:32 +0000

    - preliminary commit of alg_paramspace files

---
+++ trunk/paramspace.h
@@ -0,0 +1,41 @@
+/***************************************************************************
+                          parameter.h  -  description
+                             -------------------
+    begin                : ven ott 4 2002
+    copyright            : (C) 2002 by Davide Patti
+    email                : dpatti@diit.unict.it
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+
+#ifndef PARAMSPACE_H
+#define PARAMSPACE_H
+
+using namespace std;
+
+
+typedef struct 
+{
+    int a,b;
+} Edges;
+
+class Region {
+public: 
+  Region();
+  ~Region();
+
+
+private:
+
+ Edges edges[N_PARAMS];
+
+};
+
+#endif
