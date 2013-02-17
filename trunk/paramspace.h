762ffc6db8611b03235fcfa8ec2ee169738987b2
andrea.araldo@gmail.com <andrea.araldo@gmail.com@07f4231e-1d56-586e-1340-b6309ebb75fe>
2013-02-17 23:53:47 +0000

    before transforming vector<Region*> to vector<Region>

--- trunk/paramspace.h
+++ trunk/paramspace.h
@@ -20,6 +20,7 @@
 
 #define ALPHA 1.0 //Used to decide whether a region has high, low or no innovation
 #define DEBUG_LEVEL_DEBUG 1
+#define SEVERE_DEBUG
 
 
 using namespace std;
@@ -57,23 +58,24 @@ private:
 */
 
 
-typedef struct{
-	double id;
-	double innovation_score;
-	bool valid;
-
-//	Region()
-//  ~Region();
-
+class Region{
+	public:
+		double id;
+		double innovation_score;
+		bool valid;
 	
-	// The simulations of the current era that fall into this region
-//	list<Simulation> simulations; //PENSO CHE SIA INUTILE
+	// the index is the one computed by Parameter::get_index()
+		Edges edges[N_PARAMS];
 
-
-// the index is the one computed by Parameter::get_index()
-	Edges edges[N_PARAMS];
-
-} Region;
+		Region(){
+			id=0; innovation_score=0; valid=false;
+			for (int i=0; i<N_PARAMS; i++){
+				edges[i].a=0; edges[i].b=0;
+			}
+		}
+	
+//  ~Region();
+};
 
 
 
