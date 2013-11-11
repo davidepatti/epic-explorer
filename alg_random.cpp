df255d450f927870b1d07b92052cde4922adea44
xedivad@gmail.com <xedivad@gmail.com@07f4231e-1d56-586e-1340-b6309ebb75fe>
2013-11-11 12:24:29 +0000

    added stats

--- alg_random.cpp
+++ alg_random.cpp
@@ -10,10 +10,14 @@ void Explorer::start_RAND(int n)
     vector<Configuration> random_space;
 
     int valid = 0;
+
     reset_sim_counter();
+    Exploration_stats stats;
+
     int my_id = get_mpi_rank();
 
     string header = "["+Options.benchmark+"_"+current_algo+"] ";
+    string filename = Options.benchmark+"_RAND_"+current_space;
     string logfile = get_base_dir()+string(EE_LOG_PATH);
     string message = header+"Building random space for " + to_string(n) + " simulations...";
     write_to_log(my_id,logfile,message);
@@ -58,13 +62,21 @@ void Explorer::start_RAND(int n)
 	}
     }
 
+    stats.space_size = get_space_size();
+    stats.start_time = time(NULL);
+
     message = header+ "Valid configurations:" + to_string(valid) + " of "+to_string(n)+" requested";
     write_to_log(my_id,logfile,message);
 
     vector<Simulation> rand_sims = simulate_space(random_space);
     vector<Simulation> pareto_set = get_pareto(rand_sims);
 
-    save_simulations(rand_sims,Options.benchmark+"_RAND_"+current_space+".exp");
-    save_simulations(pareto_set,Options.benchmark+"_RAND_"+current_space+".pareto.exp");
+    save_simulations(rand_sims,filename+".exp");
+    save_simulations(pareto_set,filename+".pareto.exp");
     
+    stats.end_time = time(NULL);
+    stats.n_sim = get_sim_counter();
+    save_stats(stats,filename+".stat");
+
+    write_to_log(my_id,logfile,"End of RANDOM simulation");
 }
